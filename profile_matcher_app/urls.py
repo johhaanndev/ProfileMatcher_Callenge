b818e84f42c8925adbce3befdf3cee23b47a51e7
johhaanndev <71643284+johhaanndev@users.noreply.github.com>
2024-12-01 20:47:49 +0100

    create get_client_config logic

--- profile_matcher_app/urls.py
+++ profile_matcher_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from profile_matcher_app.views import GetCampaign, GetPlayerProfile
+from profile_matcher_app.views import GetCampaign, GetClientConfig, GetPlayerProfile
 
 
 urlpatterns = [
-    path('get_client_config/<uuid:player_id>/', GetPlayerProfile.as_view(), name='get_player_profile'),
+    path('playerprofiles/<uuid:player_id>/', GetPlayerProfile.as_view(), name='get_player_profile'),
     path('campaigns/<uuid:campaign_id>/', GetCampaign.as_view(), name='get_campaign'),
+    path('get_client_config/<uuid:player_id>/', GetClientConfig.as_view(), name='get_client_config')
 ]
--- profile_matcher_app/views.py
+++ profile_matcher_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from datetime import datetime
 
 from profile_matcher_app.models import Campaign, PlayerProfile
 from profile_matcher_app.serializers import CampaignSerializer, PlayerProfileSerializer
@@ -14,7 +15,7 @@ class GetPlayerProfile(APIView):
             return Response(serializer.data, status=status.HTTP_200_OK)
         except PlayerProfile.DoesNotExist:
             return Response({"error": "Player not found"}, status=status.HTTP_404_NOT_FOUND)
-        
+
 class GetCampaign(APIView):
     def get(self, request, campaign_id):
         try:
@@ -23,3 +24,48 @@ class GetCampaign(APIView):
             return Response(serializer.data, status=status.HTTP_200_OK)
         except Campaign.DoesNotExist:
             return Response({"error": "Campaign not found"}, status=status.HTTP_404_NOT_FOUND)
+     
+class GetClientConfig(APIView):
+    def get(self, request, player_id):
+        try:
+            player_profile = PlayerProfile.objects.get(player_id=player_id)
+
+            campaigns = Campaign.objects.filter(
+                enabled = True,
+                start_date__lte = datetime.now(),
+                end_date__gte = datetime.now()
+            )
+
+            active_campaigns = []
+
+            for campaign in campaigns:
+                match = True
+                matchers = campaign.matchers
+
+                if 'level' in matchers:
+                    if not (matchers['level']['min'] <= player_profile.level <= matchers['level']['max']):
+                        match = False
+
+                if 'has' in matchers:
+                    if 'country' in matchers['has'] and player_profile.country not in matchers['has']['country']:
+                        match = False
+                    if 'items' in matchers['has'] and not any(item in player_profile.inventory for item in matchers['has']['items']):
+                        match = False
+
+                if 'does_not_have' in matchers:
+                    if 'items' in matchers['does_not_have'] and any(item in player_profile.inventory for item in matchers['does_not_have']['items']):
+                        match = False
+
+                if match:
+                    active_campaigns.append(campaign.name)
+                
+                if (active_campaigns):
+                    player_profile.active_campaigns.extend(active_campaigns)
+                    player_profile.save()
+
+                player_serialized = PlayerProfileSerializer(player_profile)
+
+                return Response(player_serialized.data, status=status.HTTP_200_OK)
+            
+        except PlayerProfile.DoesNotExist:
+            return Response({"error": "Player not found"}, status=status.HTTP_404_NOT_FOUND)
